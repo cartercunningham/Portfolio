405fedd06016547c38f1bd2465d4152ad9435c26
Carter C <carter.r.cunningham@gmail.com>
2018-07-10 20:15:34 -0500

    second upload - in class

---
+++ HobbyBook_Unsolved.py
@@ -0,0 +1,3 @@
+# @TODO: Your code here
+hobby_book={"Name":"Carter","Age":"34","Hobby1":"tennis","Hobby2":"running","WakeUp":{"Weekdays":"7 AM","Weekends":"whenever"}}
+print(f'{hobby_book["Name"]} was {hobby_book["Age"]} & liked {hobby_book["Hobby1"]}.')
